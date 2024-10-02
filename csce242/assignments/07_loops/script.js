143ec842f4888a02c48a731fa1bc7eb41db12357
alford <bradleyalford1@icloud.com>
2024-10-01 22:27:31 -0400

    switched assignment 7 to start counting at 1 rather than 0

--- csce242/assignments/07_loops/script.js
+++ csce242/assignments/07_loops/script.js
@@ -12,10 +12,10 @@ const drawStars = () => {
         userAlert.style.color = "var(--color-1)";
         let starDiv = document.getElementById("star-area");
         const allStars = document.querySelectorAll("#star-area div");
-        for(let i =0; i < allStars.length; i++) {
+        for(let i = 0; i < allStars.length; i++) {
             allStars[i].remove();
         }
-        for(let i = 0; i < starCount.value; i++) {
+        for(let i = 1; i <= starCount.value; i++) {
             const newStar = document.createElement("div");
             starDiv.append(newStar);
             newStar.style.color = "var(--color-6)";
