e9eb1ac045ee30b94d3de4578d1a4e4b1094ca75
Bradley Alford <bradleyalford1@icloud.com>
2025-09-09 02:39:21 -0400

    empties repo

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',           // enables `next export`
-  images: { unoptimized: true },
-  trailingSlash: true         // helps with GH Pages routing
-};
-export default nextConfig;
