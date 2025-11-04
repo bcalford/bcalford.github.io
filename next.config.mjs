af3de909c1a849274e1a2c083dee70f762e8dbe0
bcalford <bcalford@email.sc.edu>
2025-11-04 02:28:17 -0500

    new simply portfolio

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',           // static export
-    images: { unoptimized: true }, // (not using next/image now but safe)
-  };
-  export default nextConfig;
-  
